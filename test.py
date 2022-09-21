17c82d7bb8e62d5013f03125d2aa16b7a7c18f33
dankeyy <dankeyy@protonmail.com>
2022-09-21 18:33:56 +0300

    test in function and bad arguments

--- test.py
+++ test.py
@@ -7,3 +7,16 @@ bar = "bar"
 swap(foo, bar)
 
 assert foo == "bar" and bar == "foo"
+
+def f():
+    a = keeper = object()
+    b = "banana"
+    swap(a, b)
+    assert a == "banana" and b == keeper
+
+f()
+
+# correctly fails with ValueError: Bad arguments to swap:
+# swap("banana", 1)
+# swap(object(), foo)
+# swap([1,2,3], (""))
